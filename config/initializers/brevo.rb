c2a60cf23feb1587d1e6e23a1f9eba228a126b59
judahsan <baraka.judah@gmail.com>
2025-09-23 13:11:13 +0300

    fix sender email typo

--- config/initializers/brevo.rb
+++ config/initializers/brevo.rb
@@ -11,7 +11,16 @@ class BrevoDeliveryMethod
   def deliver!(mail)
     validate_mail!(mail)
     
+    Rails.logger.info "Attempting to send email to: #{mail.to.join(', ')}"
+    Rails.logger.info "Email subject: #{mail.subject}"
+    
     api_key = Rails.application.credentials.dig(:brevo, :api_key)
+    
+    if api_key.blank?
+      Rails.logger.error "Brevo API key is missing!"
+      raise "Brevo API key not configured"
+    end
+    
     uri = URI('https://api.brevo.com/v3/smtp/email')
     http = Net::HTTP.new(uri.host, uri.port)
     http.use_ssl = true
@@ -21,9 +30,10 @@ class BrevoDeliveryMethod
     request['api-key'] = api_key
     request['content-type'] = 'application/json'
 
+    sender_email = @settings[:default_sender_email] || mail.from.first
     body = {
       sender: { 
-        email: @settings[:default_sender_email] || mail.from.first,
+        email: sender_email,
         name: @settings[:sender_name] || 'Africa Ruby Community'
       },
       to: mail.to.map { |email| { email: email } },
@@ -32,10 +42,16 @@ class BrevoDeliveryMethod
       textContent: mail.text_part&.body&.to_s || mail.body.to_s
     }
 
+    Rails.logger.info "Sending email from: #{sender_email}"
+    
     request.body = body.to_json
     response = http.request(request)
 
-    unless response.code.to_i.between?(200, 299)
+    Rails.logger.info "Brevo API Response: #{response.code}"
+    
+    if response.code.to_i.between?(200, 299)
+      Rails.logger.info "Email sent successfully"
+    else
       Rails.logger.error "Brevo API Error: #{response.code} - #{response.body}"
       raise "Email delivery failed: #{response.code} - #{response.body}"
     end
@@ -52,6 +68,6 @@ class BrevoDeliveryMethod
 end
 
 ActionMailer::Base.add_delivery_method :brevo, BrevoDeliveryMethod, {
-  default_sender_email: 'noreply@rubycommunity.africa',
+  default_sender_email: 'no-reply@rubycommunity.africa',
   sender_name: 'Africa Ruby Community'
 }
